70abe3e04ae04c02ae7ac463ed7b18b9808944ce
Suzan El-Gabry <suzanel-gabry@Suzans-MacBook-Pro.local>
2014-07-15 14:44:19 +0200

    done with this exercise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,21 @@
 class User < ActiveRecord::Base
+
+
+has_secure_password
+before_save {|user| user.email=user.email.downcase}
+before_validation {|user| user.name="RandomName" if user.name.blank?} 
+validates :name , presence: true , length: {maximum: 40}
+VALID_EMAIL_REGEX = /\A[\w+\-.]+@[\w+\-.]+\.[a-z]+\z/i
+validates :email, presence:true , format:{with: VALID_EMAIL_REGEX},
+uniqueness: {case_sensitive: false}
+validates :password, presence: true, length: {minimum: 8}
+validates :password_confirmation, presence: true
+
+
+def self.name_longer_than_eight
+	return User.where("length(name)>8") 
 end
+
+end
+
+
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -3,24 +3,102 @@ require 'rails_helper'
 
 describe "Users" do
 
+	before{@user=User.new(name: "Example User" , email: "user@example.com"  , password: "foobarbaz", password_confirmation: "foobarbaz")}
+
+ 	subject {@user}
+ 	it {should respond_to(:name)}
+    it {should respond_to(:email)}
+    it {should respond_to(:password_digest)}
+    it {should respond_to(:password)}
+    it {should respond_to(:password_confirmation)}
+    it {should be_valid}
+
 	describe "signup page" do
+	    it "make sure that the title is entered correctly" do
+	    	visit '/signup'
+	    	page.has_title?("Demo App | Sign Up")
+	 	end
+
+    	it "make sure that the about_page header 'h1' is correct" do
+    		visit '/signup'
+
+    		within 'h1' do 
+ 				page.should have_content "Sign up" 
+     		end 
+
+    	 end
+    end
+
+       
 
 
- it "make sure that the title is entered correctly" do
-visit '/signup'
-page.has_title?("Demo App | Sign Up")
+describe "name is not present" do
+	before {@user.name=" "}
+	it {should_not be_valid}
+end
 
- end
+describe "email is not present" do 
+	before {@user.email=" "}
+	it {should_not be_valid}
+end
 
-it "make sure that the about_page header 'h1' is correct" do
-visit '/signup'
+describe "email is too long " do 
+	before {@user.name= "a" * 41}
+	it {should_not be_valid}
+end
 
- within 'h1' do 
- 	page.should have_content "Sign up" 
- end 
+describe "email format is invalid" do
+	it "should be invalid" do
+		addresses=%w[user@foo,com user_at_foo.org example.user@foo.]
+		addresses.each do |invalid_address|
+			@user.email=invalid_address
+			@user.should_not be_valid
+		end
+	end
+end
 
- end
+describe "email format is valid" do
+	it "should be valid" do
+		addresses=%w[user@foo.com A_user@foo.b.org a+b@foo.jp]
+		addresses.each do |valid_address|
+			@user.email=valid_address
+			@user.should be_valid
+		end
+	end
+end
+
+describe "when email is already taken" do
+	before do 
+		user_with_same_email=@user.dup
+		user_with_same_email.email=@user.email.upcase
+		user_with_same_email.save
+	end
+	it {should_not be_valid}
+end
 
 
-  end
+
+describe "when password is not present" do 
+	before {@user.password=@user.password_confirmation=" "}
+	it {should_not be_valid}
+
+end
+
+
+describe "when password doesnot match the confirmation" do 
+	before {@user.password_confirmation="mismatch"}
+	it {should_not be_valid}
+end
+
+describe "when passwrod confirmation is nil" do 
+	before {@user.password_confirmation= nil}
+	it {should_not be_valid}
+end
+
+describe "when password is too short" do 
+	before {@user.password=@user.password_confirmation = "a" *7}
+	it {should_not be_valid}
+end
+
+
 end
